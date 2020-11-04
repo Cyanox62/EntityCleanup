7e007bffaf44a3a43da60f6fa938953f9383bbfe
Cyanox62 <cyanox62@gmail.com>
2020-11-03 20:00:13 -0500

    Updated patch

--- EntityCleanup/RagdollPatch.cs
+++ EntityCleanup/RagdollPatch.cs
@@ -10,15 +10,14 @@ namespace EntityCleanup
 	{
 		public static bool Prefix(RagdollManager __instance, Vector3 pos, Quaternion rot, Vector3 velocity, int classId, global::PlayerStats.HitInfo ragdollInfo, bool allowRecall, string ownerID, string ownerNick, int playerId)
 		{
-			global::Role role = __instance.hub.characterClassManager.Classes.SafeGet(classId);
+			Role role = __instance.hub.characterClassManager.Classes.SafeGet(classId);
 			if (role.model_ragdoll == null) return false;
 			GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(role.model_ragdoll, pos + role.ragdoll_offset.position, Quaternion.Euler(rot.eulerAngles + role.ragdoll_offset.rotation));
 			NetworkServer.Spawn(gameObject);
-			global::Ragdoll component = gameObject.GetComponent<global::Ragdoll>();
-			if (__instance.cleanupTime > 0) component.TimeTillCleanup = __instance.cleanupTime;
-			component.Networkowner = new global::Ragdoll.Info(ownerID, ownerNick, ragdollInfo, role, playerId);
+			Ragdoll component = gameObject.GetComponent<Ragdoll>();
+			component.Networkowner = new Ragdoll.Info(ownerID, ownerNick, ragdollInfo, role, playerId);
 			component.NetworkallowRecall = allowRecall;
-			component.RpcSyncVelo(velocity);
+			component.NetworkPlayerVelo = velocity;
 			EventHandlers.coroutines.Add(Timing.RunCoroutine(EventHandlers.HandleRagdoll(gameObject)));
 
 			return false;
