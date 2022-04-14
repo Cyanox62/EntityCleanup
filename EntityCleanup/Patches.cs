4fc493991ad041a89ce42eb45cfacccce37b7080
Cyanox62 <cyanox62@gmail.com>
2022-04-14 17:09:01 -0400

    Patch typo

--- EntityCleanup/Patches.cs
+++ EntityCleanup/Patches.cs
@@ -2,6 +2,7 @@
 using HarmonyLib;
 using InventorySystem;
 using InventorySystem.Items;
+using InventorySystem.Items.Pickups;
 using MEC;
 using Mirror;
 using System;
@@ -33,20 +34,20 @@ namespace EntityCleanup
 	[HarmonyPatch(typeof(InventoryExtensions), nameof(InventoryExtensions.ServerCreatePickup))]
 	class PickupPatch
 	{
-		public static bool Prefix(ref bool __result, Inventory inv, InventorySystem.Items.ItemBase item, InventorySystem.Items.Pickups.PickupSyncInfo psi, bool spawn = true)
+		public static bool Prefix(ref ItemPickupBase __result, Inventory inv, ItemBase item, PickupSyncInfo psi, bool spawn = true)
 		{
 			if (!NetworkServer.active)
 			{
 				throw new InvalidOperationException("Method ServerCreatePickup can only be executed on the server.");
 			}
-			InventorySystem.Items.Pickups.ItemPickupBase itemPickupBase = GameObject.Instantiate<InventorySystem.Items.Pickups.ItemPickupBase>(item.PickupDropModel, inv.transform.position, global::ReferenceHub.GetHub(inv.gameObject).PlayerCameraReference.rotation * item.PickupDropModel.transform.rotation);
+			ItemPickupBase itemPickupBase = GameObject.Instantiate<InventorySystem.Items.Pickups.ItemPickupBase>(item.PickupDropModel, inv.transform.position, global::ReferenceHub.GetHub(inv.gameObject).PlayerCameraReference.rotation * item.PickupDropModel.transform.rotation);
 			itemPickupBase.NetworkInfo = psi;
 			if (spawn)
 			{
 				NetworkServer.Spawn(itemPickupBase.gameObject);
 				EventHandlers.coroutines.Add(Timing.RunCoroutine(EventHandlers.HandleDroppedItem(itemPickupBase)));
 			}
-			itemPickupBase.InfoReceived(default(InventorySystem.Items.Pickups.PickupSyncInfo), psi);
+			itemPickupBase.InfoReceived(default(PickupSyncInfo), psi);
 			__result = itemPickupBase;
 			return false;
 		}
